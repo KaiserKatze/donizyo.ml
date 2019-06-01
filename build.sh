b96af2d52be3a016062a3cc857959a99d61a22f8
KaiserKatze <donizyo@gmail.com>
2019-06-01 16:28:48 +0800

    Update

--- build.sh
+++ build.sh
@@ -1,4 +1,9 @@
-#!/bin/sh
+#!/bin/bash
+
+# Image dependencies
+# supported by bash 4
+declare -A image_dep=( ["bind"]="python" ["python"]="sqlite" ["sqlite"]="openssl" ["openssl"]="" )
+declare -A image_ver=( ["bind"]="9.14.2" ["python"]="3.7.3" ["sqlite"]="3.28.0" ["openssl"]="1.1.0k" )
 
 pull() {
     image=$1
@@ -20,10 +25,30 @@ build() {
     docker push         $repo
 }
 
-build_all() {
+build_base() {
     docker build -t base ./base
+}
+
+build_all() {
+    build_base
     build openssl   1.1.0k
     build sqlite    3.28.0
     build python    3.7.3
     build bind      9.14.2
+}
+
+build_only() {
+    image=$1
+    if [ -z "$image" ]; then return; fi
+    iter=${image_dep["$image"]}
+    list="$image"
+    # while `$iter` is not empty string
+    while [ -n "$iter" ]; do
+        list="$iter $list"
+        iter=${image_dep["$iter"]}
+    done
+    build_base
+    for image in $list; do
+        build $image ${image_ver["$image"]}
+    done
 }
