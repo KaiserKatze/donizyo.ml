46e2684544c754cd7c4e30aa586928f2862bd8ff
KaiserKatze <donizyo@gmail.com>
2019-06-08 20:40:43 +0800

    Update

--- run.sh
+++ run.sh
@@ -10,36 +10,40 @@ trap onerror ERR
 
 # setup firewall before docker does so
 
-# delete all containers
-docker rm $(docker ps -a -q)
+start() {
+    # delete all containers
+    docker rm -f $(docker ps -a -q)
 
-hostname=matrix
+    # create new containers
 
-# create new containers
+    # setup dns service
+    docker run -d \
+        -p 127.0.0.1:53:53/udp \
+        --name dns \
+        bind \
+        named -g -u bind
 
-# setup dns service
-docker run -d \
-    -p 127.0.0.1:53:53/udp \
-    --name dns \
-    bind \
-    named -g -u bind
+    # obtain https certificate
+    # @see: https://certbot.eff.org/docs/install.html#running-with-docker
+    docker run -it --rm --name certbot \
+        -p 127.0.0.1:80:80/tcp \
+        -p 127.0.0.1:443:443/tcp \
+        -v "/etc/letsencrypt:/etc/letsencrypt" \
+        -v "/var/lib/letsencrypt:/var/lib/letsencrypt" \
+        certbot/certbot certonly
 
-# obtain https certificate
-# @see: https://certbot.eff.org/docs/install.html#running-with-docker
-docker run -it --rm --name certbot \
-    -p 127.0.0.1:80:80/tcp \
-    -p 127.0.0.1:443:443/tcp \
-    -v "/etc/letsencrypt:/etc/letsencrypt" \
-    -v "/var/lib/letsencrypt:/var/lib/letsencrypt" \
-    certbot/certbot certonly
+    # setup web service
+    docker run -d \
+        -p 127.0.0.1:80:80/tcp \
+        -p 127.0.0.1:443:443/tcp \
+        -p 127.0.0.1:1935:1935/tcp \
+        -v "/etc/letsencrypt:/etc/letsencrypt" \
+        -v "/var/lib/letsencrypt:/var/lib/letsencrypt" \
+        --name web \
+        nginx \
+        nginx -g 'daemon off;'
+}
 
-# setup web service
-docker run -d \
-    -p 127.0.0.1:80:80/tcp \
-    -p 127.0.0.1:443:443/tcp \
-    -p 127.0.0.1:1935:1935/tcp \
-    -v "/etc/letsencrypt:/etc/letsencrypt" \
-    -v "/var/lib/letsencrypt:/var/lib/letsencrypt" \
-    --name web \
-    nginx \
-    nginx -g 'daemon off;'
+finalize() {
+    docker rm -f dns
+}
