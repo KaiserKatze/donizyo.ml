5d37ac5a6bfbcfcda25f8b67a69b2e0c8431ecbe
KaiserKatze <donizyo@gmail.com>
2019-06-13 19:13:29 +0800

    Update

--- run.sh
+++ run.sh
@@ -8,6 +8,24 @@ onerror() {
 # exit 1 on error
 trap onerror ERR
 
+install_docker() {
+    # @see: https://docs.docker.com/install/linux/docker-ce/ubuntu/
+    apt-get update
+    apt-get -y install \
+        apt-transport-https \
+        ca-certificates \
+        curl \
+        gnupg-agent \
+        software-properties-common
+    curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+    add-apt-repository \
+        "deb [arch=amd64] https://download.docker.com/linux/ubuntu \
+        $(lsb_release -cs) \
+        stable"
+    apt-get update
+    apt-get -y install docker-ce docker-ce-cli containerd.io
+}
+
 # setup firewall before docker does so
 
 disable_docker_firewall() {
