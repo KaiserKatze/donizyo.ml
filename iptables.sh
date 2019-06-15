0959ada5c96e106e080c87a6537fbec62174d0b7
KaiserKatze <donizyo@gmail.com>
2019-06-15 10:11:19 +0800

    Update

--- iptables.sh
+++ iptables.sh
@@ -92,6 +92,17 @@ if [ -z "$LO_IFACE" ]; then exit 1; fi
 
 # there is no need to publish SSH on port 22
 PORT_SSH=22
+# try to parse sshd config and figure out true port on which sshd listens
+path_sshd_config=/etc/ssh/sshd_config
+if [ -f "$path_sshd_config" ];
+then
+    sshd_listen_addr=$(cat $path_sshd_config | awk '/^\s*ListenAddress/{print $2}')
+    sshd_ext_if_port=$(echo $sshd_listen_addr | grep -Po "$INET_ADDRESS:\K\d+")
+    sshd_all_if_port=$(echo $sshd_listen_addr | grep -Po '0\.0\.0\.0:\K\d+')
+    if [ -n "$sshd_ext_if_port" ]; then PORT_SSH=$sshd_ext_if_port; fi
+    if [ -n "$sshd_all_if_port" ]; then PORT_SSH=$sshd_all_if_port; fi
+    if [ -z "$PORT_SSH" ]; then PORT_SSH=22; fi
+fi
 
 ###############################################################################
 #
