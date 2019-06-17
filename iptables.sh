e2db86a964e7af41ae9ffec370bc221a1c14ab60
KaiserKatze <donizyo@gmail.com>
2019-06-17 18:16:46 +0800

    Update

--- iptables.sh
+++ iptables.sh
@@ -45,6 +45,7 @@ failure() {
     local lineno=$1
     local msg=$2
     echo -e "\e[91mFailed\e[0m at $lineno: $msg\n" \
+        "Five most recent command:\n" \
         "$previous_command"
 }
 trap 'previous_command=$this_command; this_command=$BASH_COMMAND' DEBUG
